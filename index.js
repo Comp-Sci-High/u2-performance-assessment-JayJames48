6a0150bb4e93fd5e18d6ecde95a716c47dcd05fc
JayJames48 <jayden.james25@compscihigh.org>
2024-11-18 17:33:04 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+async function fetchrecipe(){
+    const response = await fetch ("https://api.spoonacular.com/recipes/1003464/information?apiKey=09d315013db244fa950dbb5656dac380")
+        const data = await response.json()
+        console.log(data)
+        return data 
+      }
+       fetchrecipe()
+
+    async function fetchimage(){
+        const options = {}
+            method:`post`,
+            Headers{
+             Authorization:
+            }
+        }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
 
